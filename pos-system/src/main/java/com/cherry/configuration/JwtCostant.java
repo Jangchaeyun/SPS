0a94a3be107009e321a6364ce28f7d53662d7d86
Jangchaeyun <zzangsally@naver.com>
2025-09-20 23:11:43 +0900

    0920 SecurityConfig & JWT

---
+++ pos-system/src/main/java/com/cherry/configuration/JwtCostant.java
@@ -0,0 +1,6 @@
+package com.cherry.configuration;
+
+public class JwtCostant {
+    public static final String JWT_SECRET = "cd2a1e93d3604b9322c0d134a83e5a81";
+    public static final String JWT_HEADER = "Authorization";
+}
