a2cac32b90cb2e7fd7d358c5714919744f23eea2
Jangchaeyun <zzangsally@naver.com>
2025-09-21 22:52:13 +0900

    0921 User & UserRepository & CustomUserImplementation

---
+++ pos-system/src/main/java/com/cherry/service/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.cherry.service.repository;
+
+import com.cherry.modal.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+}
