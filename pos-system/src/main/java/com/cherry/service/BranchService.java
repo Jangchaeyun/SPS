3af187ccf9879a807d7db1698ee87117c05512f0
Jangchaeyun <zzangsally@naver.com>
2025-10-16 22:59:50 +0900

    1004 Update BranchService & BranchServiceImpl

--- pos-system/src/main/java/com/cherry/service/BranchService.java
+++ pos-system/src/main/java/com/cherry/service/BranchService.java
@@ -1,14 +1,15 @@
 package com.cherry.service;
 
+import com.cherry.exceptions.UserException;
 import com.cherry.modal.User;
 import com.cherry.payload.dto.BranchDTO;
 
 import java.util.List;
 
 public interface BranchService {
-    BranchDTO createBranch(BranchDTO branchDTO, User user);
-    BranchDTO updateBranch(Long id, BranchDTO branchDTO, User user);
-    BranchDTO deleteBranch(Long id);
+    BranchDTO createBranch(BranchDTO branchDTO, User user) throws UserException;
+    BranchDTO updateBranch(Long id, BranchDTO branchDTO, User user) throws Exception;
+    void deleteBranch(Long id) throws Exception;
     List<BranchDTO> getAllBranchesByStoreId(Long storeId);
-    BranchDTO getBranchById(Long id);
+    BranchDTO getBranchById(Long id) throws Exception;
 }
--- pos-system/src/main/java/com/cherry/service/Impl/BranchServiceImpl.java
+++ pos-system/src/main/java/com/cherry/service/Impl/BranchServiceImpl.java
@@ -1,41 +1,80 @@
 package com.cherry.service.Impl;
 
+import com.cherry.exceptions.UserException;
+import com.cherry.mapper.BranchMapper;
+import com.cherry.modal.Branch;
+import com.cherry.modal.Store;
 import com.cherry.modal.User;
 import com.cherry.payload.dto.BranchDTO;
 import com.cherry.repository.BranchRepository;
+import com.cherry.repository.UserRepository;
 import com.cherry.service.BranchService;
+import com.cherry.service.UserService;
+import com.cherry.service.repository.StoreRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class BranchServiceImpl implements BranchService {
     private final BranchRepository branchRepository;
+    private final StoreRepository storeRepository;
+    private final UserRepository userRepository;
+    private final UserService userService;
 
     @Override
-    public BranchDTO createBranch(BranchDTO branchDTO, User user) {
-        return null;
+    public BranchDTO createBranch(BranchDTO branchDTO, User user) throws UserException {
+        User currentUser = userService.getCurrentUser();
+        Store store = storeRepository.findByStoreAdminId(currentUser.getId());
+
+        Branch branch = BranchMapper.toEntity(branchDTO, store);
+        Branch savedBranch = branchRepository.save(branch);
+        return BranchMapper.toBranchDTO(savedBranch);
     }
 
     @Override
-    public BranchDTO updateBranch(Long id, BranchDTO branchDTO, User user) {
-        return null;
+    public BranchDTO updateBranch(Long id, BranchDTO branchDTO, User user) throws Exception {
+        Branch exiting = branchRepository.findById(id).orElseThrow(
+                () -> new Exception("branch not exist...")
+        );
+
+        exiting.setName(branchDTO.getName());
+        exiting.setWorkingDays(branchDTO.getWorkingDays());
+        exiting.setEmail(branchDTO.getEmail());
+        exiting.setPhone(branchDTO.getPhone());
+        exiting.setAddress(branchDTO.getAddress());
+        exiting.setOpenTime(branchDTO.getOpenTime());
+        exiting.setCloseTime(branchDTO.getCloseTime());
+        exiting.setUpdatedAt(LocalDateTime.now());
+
+        Branch updatedBranch = branchRepository.save(exiting);
+        return BranchMapper.toBranchDTO(updatedBranch);
     }
 
     @Override
-    public BranchDTO deleteBranch(Long id) {
-        return null;
+    public void deleteBranch(Long id) throws Exception {
+        Branch exiting = branchRepository.findById(id).orElseThrow(
+                () -> new Exception("branch not exist...")
+        );
+        branchRepository.delete(exiting);
     }
 
     @Override
     public List<BranchDTO> getAllBranchesByStoreId(Long storeId) {
-        return List.of();
+        List<Branch> branches =  branchRepository.findByStoreId(storeId);
+        return branches.stream().map(BranchMapper::toBranchDTO)
+                .collect(Collectors.toList());
     }
 
     @Override
-    public BranchDTO getBranchById(Long id) {
-        return null;
+    public BranchDTO getBranchById(Long id) throws Exception {
+        Branch exiting = branchRepository.findById(id).orElseThrow(
+                () -> new Exception("branch not exist...")
+        );
+        return BranchMapper.toBranchDTO(exiting);
     }
 }
